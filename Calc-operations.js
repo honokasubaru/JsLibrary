c6959cefb32b11175bae2d9b86b70689f73e152d
BrayanPuentes <brayansmithpuente@gmail.com>
2020-12-07 19:33:27 -0500

    commit callarray

--- Calc-operations.js
+++ Calc-operations.js
@@ -9,15 +9,13 @@ class CalcArray {
 
     averageOfArray(array) {
         //Algoritm for obtain a average of 1 array.
-        if (typeof(array) == Object) {
-            let average = 0;
-            for (let i in array) {
-                average += array[i];
-            }
-            return average / array.length;
+        console.log(typeof(array));
 
-        } else return console.log("Parameter is not an array");
-
+        let average = 0;
+        for (let i in array) {
+            average += array[i];
+        }
+        return average / array.length;
     }
 }
 
