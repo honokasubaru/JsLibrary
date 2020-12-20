059460e7504e60bf948bcc3085095e438cf5db98
BrayanPuentes <brayansmithpuente@gmail.com>
2020-12-19 23:39:05 -0500

    first commit array methods basics01

--- devmain.js
+++ devmain.js
@@ -2,3 +2,47 @@
 
 
 /* Arrays tools*/
+/* Array methods*/
+class CalcArray {
+    //calculate operations with arrays.
+    constructor() {}
+
+    adition(array) {
+        let addArray = 0;
+        for (let i in array) {
+            addArray += array[i];
+        }
+        return addArray;
+    }
+
+    average(array) {
+        //Algoritm for obtain a average of an array.
+
+        let average = 0;
+        for (let i in array) {
+            average += array[i];
+        }
+        return average / array.length;
+    }
+
+    maximunValue(array) {
+
+        //Obtain maximun value of an array
+
+        let maximun = 0;
+        for (let i in array) {
+            if (maximun < array[i]) {
+                maximun = array[i];
+            }
+        }
+
+        return maximun;
+    }
+
+}
+
+//objects to export. 
+
+const ofArray = new CalcArray();
+
+//Testing zone.
