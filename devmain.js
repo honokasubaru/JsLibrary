cec214b365f8fe306d6731c53f33caab17cec0a2
Brayan Puentes Ruiz <63372595+SMITH367@users.noreply.github.com>
2020-12-25 17:24:01 -0500

    Update devmain.js

--- devmain.js
+++ devmain.js
@@ -1,14 +1,18 @@
-//Creator of method for easy tolls
+/*
+Easy tools, simple and usefull library
+*/
+
+
 
 
 /* Arrays tools*/
-/* Array methods*/
+
 class CalcArray {
-    //calculate operations with arrays.
+    //calculate operations and tolls for use with arrays.
     constructor() {}
 
     adition(array) {
-        //Obtain the add of all elements of an array
+        //Obtain the add of all elements of an array 
         let addArray = 0;
         for (let i in array) {
             addArray += array[i];
@@ -39,7 +43,6 @@ class CalcArray {
 
         return maximun;
     }
-
     minimunValue(array) {
 
         //Obtain minimun value of an array
@@ -55,27 +58,97 @@ class CalcArray {
         return minimun;
     }
 
-    bubbleSort(array){
-        //sort the array with bubble sorting algorithm 
-        let temp;
 
-        for (let i = 0; i < array.length;i++) {
-            for (let j = 0; j < array.length-i; j++) {
-                if(array[j]>array[j+1]){
-                    temp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = temp;                
-                }                
-            }            
+    generateArray(numberof) {
+
+        //Generating an array
+
+        let array = [];
+        if (numberof < 1000000) {
+            let generator;
+            console.log(numberof);
+            for (let i = 0; i < numberof; i++) {
+
+                generator = parseInt(Math.random() * 100);
+                array.push(generator);
+            }
+            return array;
         }
-        return array;
+
     }
+    arrayTObject(array) {
+
+        //return an object with index = id of array and value is the argument of array
+
+        let object = {};
+
+        for (let i in array) {
+
+            object[i] = array[i];
+
+        }
+        return object;
+    }
+
 }
 
-//objects to export. 
+
+
+
+
+
+const quickShort = (array) => {
+
+    //Algorithm quickshort for order arrays
+
+    if (array.length <= 1) {
+        return array;
+    }
+    var index = array[0];
+
+    var left = [];
+    var right = [];
+
+    for (var i = 1; i < array.length; i++) {
+        if (array[i] < index) {
+
+            left.push(array[i]);
+
+        } else {
+
+            right.push(array[i]);
+
+        }
+    }
+    return quickShort(left).concat(index, quickShort(right));
+};
+
+const bubbleSort = (array) => {
+    //sort the array with bubble sorting algorithm 
+    let temp;
+
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length - i; j++) {
+            if (array[j] > array[j + 1]) {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    return array;
+}
+
+
+//Objects to export. 
 
 const ofArray = new CalcArray();
-console.log(ofArray.bubbleSort([4, 2, -1, 2, 12]));
+console.log(ofArray.arrayTObject(["juan", "brayan", "keila", "amateratsu", "gato"]));
+
+
+
+//Exporting all objects and functions
+
 
 
 //Testing zone.
